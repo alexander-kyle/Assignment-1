df174b2b028007b1a9bbcfd4214d3787ac9271b7
Vithusan <vithusan_ak@hotmail.com>
2016-02-14 22:13:18 -0800

    Added some css styling and edited assembly page to pull images, but it didn't work Signed-off-by:Vithusan <vithusan_ak@hotmail.com>

--- application/views/portfolio_owned.php
+++ application/views/portfolio_owned.php
@@ -5,28 +5,64 @@
     //
     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
     ?>
+<style type="text/css">  
+        .Contents {
+		
+		width: auto;
+                height: 100%;
+                
+                
+                
+	}
+        
+       
+        div#box {   
+            margin-left: auto;
+            margin-right: auto;
+            width: 45%;
+        }
+        
+        div#box .box-title {
+            color: #fff;
+            text-shadow: 0px 0px 1px #ddd;
+            background-color: #3fb0ac;
+            padding: 5px;
+        }
+        div#box .box-content {
+            padding: 80px;
+            background-color: #f6f1ed;
+            border: solid #ccc;
+        }
+        
+</style>
+                
+            <div class="Contents">
+                <h2></h2>    
+                <div id="box">
+                    <div class="box-title">{portfolioOfPlayer}'s Pieces</div>
+                    <div class="box-content">
+                        <table border="1" width="100%" style="border-collapse: collapse;">
+                            <tr>
+                                <th/><th>Series 11</th><th>Series 13</th>
+                                <th>Series 26</th>
+                            </tr>
+                            <tr>
+                                <th>Heads</th><td>{s11Heads}</td><td>{s13Heads}</td>
+                                <td>{s26Heads}</td>
+                            </tr>
+                            <tr>
+                                <th >Bodies</th><td>{s11Bodies}</td><td>{s13Bodies}</td>
+                                <td>{s26Bodies}</td>
+                            </tr>
+                            <tr>
+                                <th>Legs</th><td>{s11Legs}</td><td>{s13Legs}</td>
+                                <td>{s26Legs}</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+                    
+            </div>
 
-<div style="float: right;">
-    <h2>{portfolioOfPlayer}'s Pieces</h2>
-    <br/>
-    <table border="1" width="100%" style="border-collapse: collapse;">
-        <tr>
-            <th/><th>Series 11</th><th>Series 13</th>
-            <th>Series 26</th>
-        </tr>
-        <tr>
-            <th>Heads</th><td>{s11Heads}</td><td>{s13Heads}</td>
-            <td>{s26Heads}</td>
-        </tr>
-        <tr>
-            <th >Bodies</th><td>{s11Bodies}</td><td>{s13Bodies}</td>
-            <td>{s26Bodies}</td>
-        </tr>
-        <tr>
-            <th>Legs</th><td>{s11Legs}</td><td>{s13Legs}</td>
-            <td>{s26Legs}</td>
-        </tr>
-    </table>
-</div>
 
 
--- application/views/portfolio_recent.php
+++ application/views/portfolio_recent.php
@@ -5,18 +5,51 @@
     //
     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
     ?>
-<div style="float: left;"
-     <h2>{portfolioOfPlayer}'s Recent Activity</h2>
-    <br/>
-    {activities}
-    <table border="1" width="100%" style="border-collapse: collapse;">
-        <tr>
-            <td>
-                {Trans} a {Series} bot piece on {DateTime}
-            </td>
-        </tr>
-    </table>
-    <br/>
-    {/activities}
-</div>
+<style type="text/css">  
+        .Contents {
+		width: auto;
+                height: 100%;
+	}
+        
+       
+        div#box {   
+            margin-left: auto;
+            margin-right: auto;
+            width: 45%;
+        }
+        
+        div#box .box-title {
+            color: #fff;
+            text-shadow: 0px 0px 1px #ddd;
+            background-color: #3fb0ac;
+            padding: 5px;
+        }
+        div#box .box-content {
+            padding: 80px;
+            background-color: #f6f1ed;
+            border: solid #ccc;
+        }
+        
+</style>
+                
+            <div class="Contents">
+                    
+                <div id="box">
+                    <div class="box-title">{portfolioOfPlayer}'s Recent Activity</div>
+                    <div class="box-content">
+                        {activities}
+                            <table border="1" width="100%" style="border-collapse: collapse;">
+                                <tr>
+                                    <td>
+                                        {Trans} a {Series} bot piece on {DateTime}
+                                    </td>
+                                </tr>
+                            </table>
+                            <br/>
+                        {/activities}
+                    </div>
+                </div>
+                    
+            </div>
+
 
